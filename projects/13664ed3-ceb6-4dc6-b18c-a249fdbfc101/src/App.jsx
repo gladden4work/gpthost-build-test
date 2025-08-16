6a368ea95bacb99d4ce3b65cdcede9aa847c645d
gladden4work <71418882+gladden4work@users.noreply.github.com>
2025-08-16 10:59:10 +0800

    Upload source files for project 13664ed3-ceb6-4dc6-b18c-a249fdbfc101

---
+++ projects/13664ed3-ceb6-4dc6-b18c-a249fdbfc101/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Component from './components/Component';
+import './App.css'
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Component />
+      </header>
+    </div>
+  );
+}
+
+export default App;
