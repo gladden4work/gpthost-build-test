4f9c0de209322cfba938a2eed48f077f1f83f5cb
gladden4work <71418882+gladden4work@users.noreply.github.com>
2025-08-16 17:11:51 +0800

    Upload source files for project 029c3047-2aa4-4052-81f7-292e5d5e89d1

---
+++ projects/029c3047-2aa4-4052-81f7-292e5d5e89d1/src/components/component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TestApp() {
+  return (
+    <div>
+      <h1>MVP Complete Test</h1>
+      <p>Testing with fixed workflow that uses uploaded files</p>
+    </div>
+  );
+}
+
+export default TestApp;
