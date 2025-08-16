4ad28da00ef49975f85e1d00644b8271d28c6b97
gladden4work <71418882+gladden4work@users.noreply.github.com>
2025-08-16 17:06:41 +0800

    Upload source files for project e6e2dc5d-228c-48cd-b8bc-6fa24e0d06a9

---
+++ projects/e6e2dc5d-228c-48cd-b8bc-6fa24e0d06a9/src/components/component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>MVP E2E Test</h1>
+      <p>Testing complete pipeline with fixed workflow</p>
+    </div>
+  );
+}
+
+export default App;
